cbf8d4dfb160f36dc7b9ba5b4a75c34b9184a805
deadmau5v <deadmau5v@qq.com>
2023-12-18 01:47:08 +0800

    update 睡觉

--- 链家/djangoProject/test1/views.py
+++ 链家/djangoProject/test1/views.py
@@ -14,13 +14,40 @@ def index(request):
 
 
 def echarts1(request):
-    data1 = df[["所在楼层"]]
-    data2 = data1.map(lambda x: x[0:3])
+    data1 = df["所在楼层"]
+    # 截取楼层大概位置 比如 低楼层 中楼层 高楼层
+    data2 = data1.map(lambda x: x[:3])
+
     value_counts = data2.value_counts().to_list()
     pie_data = [("低楼层", value_counts[0]), ("中楼层", value_counts[1]), ("高楼层", value_counts[2])]
     pie = pyecharts.charts.Pie()
-    pie.add("", pie_data)
-    pie.render("./templates/echarts1.html")
+    pie.add("楼层等级分布", pie_data)
+
+    # 截取楼层的具体位置 比如 1楼 2楼 3楼
+    data3 = data1.map(lambda x: x[3:])
+    data3 = data3.map(lambda x: x
+                      .replace("(", "")
+                      .replace(")", "")
+                      .replace(" ", "")
+                      .replace("共", "")
+                      .replace("层", "")
+                      )
+    data3 = data3.map(lambda x: int(x))
+
+    # 将楼层分为5层一组
+    def to_str(x):
+        r = int(x / 5) * 5
+        return f"{r}~{r + 5}层"
+    data3 = data3.map(to_str)
+
+    pie_2 = pyecharts.charts.Pie()
+    pie_2.add("具体楼层分布", list(data3.value_counts().to_dict().items()))
+
+    table = pyecharts.charts.Tab()
+    table.add(pie, "楼层等级分布")
+    table.add(pie_2, "具体楼层分布")
+
+    table.render("./templates/echarts1.html")
 
     data = {
         "title": "房源楼层分布图",
@@ -36,12 +63,9 @@ def echarts2(request):
     scatter = pyecharts.charts.Scatter()
     scatter.add_xaxis(data1["金额"])
     scatter.add_yaxis("二手房价格/面积", data1["建筑面积"])
-    scatter.set_series_opts(
-        label_opts=pyecharts.options.series_options.LabelOpts(is_show=False),
-    )
     scatter.set_global_opts(
-        yaxis_opts=pyecharts.options.AxisOpts(max_=data1["建筑面积"].max(), name="面积"),
-        xaxis_opts=pyecharts.options.AxisOpts(max_=data1["金额"].max(), name="金额")
+        xaxis_opts=pyecharts.options.global_options.AxisOpts(name="价格"),
+        yaxis_opts=pyecharts.options.global_options.AxisOpts(name="面积")
     )
     scatter.render("./templates/echarts2.html")
     data = {
